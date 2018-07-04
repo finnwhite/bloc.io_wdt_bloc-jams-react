0a17aea1a116081d3ec8e66ddb7375a5a7c7a63f
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-07-04 11:58:05 -0700

    Create Landing, Library routes
    Added static placeholders for main content sections:
    
    - Installed React Router (react-router-dom)
    
    - Created dummy Landing, Library components/routes
    
    - Added basic navigation menu
    
    Completes Bloc Jams React > CP03 Routing

---
+++ src/components/Library.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Library extends Component {
+  render() {
+    return (
+      <section className="library">
+        Library page goes here.
+      </section>
+    );
+  }
+}
+
+export default Library;
