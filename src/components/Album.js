54f919efa6bc96e0fe1a07af371d745824000d64
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-07-06 14:06:19 -0700

    Update Album page, dynamic content
    Completes Bloc Jams React > CP06 Components: Album
    
    See: #3

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+
+    const slug = this.props.match.params.slug;
+    const album = albumData.find( (album) => {
+      return (album.slug === slug);
+    });
+
+    this.state = { album: album };
+  }
+
   render() {
+    const album = this.state.album;
     return (
       <section className="album">
-        { this.props.match.params.slug } Album page goes here.
+
+        <section id="album-info">
+          <img id="album-cover-art"
+            src={ album.albumCover } alt={ album.title } />
+          <div className="album-details">
+            <h1 id="album-title">{ album.title }</h1>
+            <h2 className="artist">{ album.artist }</h2>
+            <div id="release-info">{ album.releaseInfo }</div>
+          </div>
+        </section>
+
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+          </tbody>
+        </table>
+
       </section>
     );
   }
